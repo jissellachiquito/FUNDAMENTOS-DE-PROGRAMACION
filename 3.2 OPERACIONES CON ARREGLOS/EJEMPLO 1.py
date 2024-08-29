18a961d1cd8a8fe94def14992dec5ebaa2fcf5e6
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2024-08-29 16:41:22 -0500

    TAREA DE LA SEMANA 11

---
+++ 3.2 OPERACIONES CON ARREGLOS/EJEMPLO 1.py
@@ -0,0 +1,22 @@
+#REALIZAR UNA MATRIZ DE 3X3
+matriz = [
+    [33, 78, 67],
+    [20, 50, 89],
+    [23, 39, 19]
+]
+print(matriz)
+# funcion buscar un valor especifico
+def buscar_valor(matriz,valor_buscado):
+    for i in range(len(matriz)):
+        for j in range(len(matriz)):
+            if matriz[i][j] == valor_buscado:
+                return i,j
+
+valor_buscado = 19
+print(buscar_valor(matriz,valor_buscado))
+
+if valor_buscado == valor_buscado:
+    print(valor_buscado,',valor correcto,encontrado en la posicion',buscar_valor(matriz,valor_buscado))
+else:
+    print('valor incorrecto')
+
---
+++ 3.2 OPERACIONES CON ARREGLOS/EJEMPLO 2.py
@@ -0,0 +1,6 @@
+#REALIZAR UNA MATRIZ DE 3X3
+matriz = [
+    [14, 90, 8],
+    [62, 21, 39],
+    [84, 39, 54]
+]
