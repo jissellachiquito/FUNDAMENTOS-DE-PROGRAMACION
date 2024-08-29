05fc46fa85c2ae0bf004565216e2887a628c31cb
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2024-08-29 17:42:30 -0500

    TAREA DE LA SEMANA 11

--- 3.2 OPERACIONES CON ARREGLOS/EJEMPLO 2.py
+++ 3.2 OPERACIONES CON ARREGLOS/EJEMPLO 2.py
@@ -1,6 +1,22 @@
 #REALIZAR UNA MATRIZ DE 3X3
 matriz = [
-    [14, 90, 8],
-    [62, 21, 39],
-    [84, 39, 54]
+    [4, 15, 8],
+    [6, 21, 9],
+    [8, 3, 5]
 ]
+print(matriz)
+#metodo de ordenamiento buble_sort
+def buble_sort(fila):
+    #algoritmo buble_sort
+    n = len(fila)
+    for i in range(n):
+        for j in range(n-1,i,-1):
+            if fila[j] > fila[j-1]:
+                fila[j], fila[j - 1] = fila[j - 1], fila[j]
+                print(fila)
+print('matriz original')
+print(matriz)
+buble_sort(matriz[1])
+print(matriz)
+
+
