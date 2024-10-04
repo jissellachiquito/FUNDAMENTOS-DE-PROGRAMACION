f8401023a17901d493784c492cd0d69a0340ff51
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2024-10-04 00:27:33 -0500

    tareas con notas personales

---
+++ 3.2 OPERACIONES CON ARREGLOS/TAREA SEMANA 16/my_notes.txt.py
@@ -0,0 +1,20 @@
+
+# Crear y escribir en el archivo my_notes.txt
+with open('my_notes.txt', 'w') as file:  # Abre el archivo
+    file.write("Estas son mis notas personales:\n")
+    file.write("Tarea 1: Hacer la compra \n")  # la primera línea
+    file.write("Tarea 2: Estudiar para el examen \n")  # la segunda línea
+    file.write("Tarea 3: Limpiar la casa \n")  # la tercera línea
+
+# Leer el archivo my_notes.txt
+with open('my_notes.txt', 'r') as file:# Abre el archivo
+    # Leer cada línea del archivo
+    line = file.readline()  # Lee la primera línea
+    while line:  # Mientras haya líneas lee
+        print(line.strip())  # Imprime la línea sin saltos
+        line = file.readline()  # Lee la siguiente línea
+
+# El archivo se cierra automáticamente al salir del bloque with
+
+
+
